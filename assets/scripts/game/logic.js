dbafba7b52764aa63b267f7302e71761a2c291ec
nicholaspowel <nicholaspowel@gmail.com>
2019-08-14 11:02:31 -0400

    Create placeholder logic functions
    Created empty functions for the game logic

---
+++ assets/scripts/game/logic.js
@@ -0,0 +1,21 @@
+const store = require('../store')
+
+const placePiece = () => {}
+const checkRow = () => {}
+const checkColumn = () => {}
+const checkDiagonal = () => {}
+const checkWin = () => {}
+const checkTeam = () => {}
+const reset = () => {}
+const start = () => {}
+
+module.exports = {
+  placePiece,
+  checkRow,
+  checkColumn,
+  checkDiagonal,
+  checkWin,
+  checkTeam,
+  reset,
+  start
+}
--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const store = {
+  board: null,
 }
 
 module.exports = store
