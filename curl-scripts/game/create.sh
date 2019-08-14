559eceaf400c91651efdd68dcc929ad00b84d2e4
nicholaspowel <nicholaspowel@gmail.com>
2019-08-14 14:43:31 -0400

    Finish Game Curl scripts
    Finished making the curl scripts for modifying the game

---
+++ curl-scripts/game/create.sh
@@ -0,0 +1,10 @@
+# VARIABLE=VALUE sh curl-scripts/examples/create.sh
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/games" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"\
+  --data '{}'
+
+echo
