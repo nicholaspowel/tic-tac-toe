30c92e616eb867a457a8ea40d3bce9cf3ff7dfb9
nicholaspowel <nicholaspowel@gmail.com>
2019-08-27 18:22:29 -0400

    Begin filling out notice methods

--- assets/scripts/helpers/notice.js
+++ assets/scripts/helpers/notice.js
@@ -1,7 +1,17 @@
 'use strict'
 
+const store = require('./../store.js')
+const logic = require('./../games/logic.js')
 
+const message = (target = '#message', message = 'Success!', isSuccess = true) => {
+  const style = isSuccess ? 'success' : 'failure'
+  $(target).text(message)
+  $(target).removeClass()
+  // try $(target).className('success')
+  $(target).addClass(style) // optional: adds css class for styling
+  $('form').trigger('reset')
+}
 
 module.exports = {
-
+  message
 }
